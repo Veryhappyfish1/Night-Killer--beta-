ee1105c8c0ec69982af0bc8247003dd1420c8012
CavemanGaming <126614429+Veryhappyfish1@users.noreply.github.com>
2026-02-19 02:50:40 -0600

    Cool stuff

---
+++ night killer web/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "RLP-Night Killer-3";
+const contentToCache = [
+    "Build/night killer web.loader.js",
+    "Build/night killer web.framework.js.gz",
+    "Build/night killer web.data.gz",
+    "Build/night killer web.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
